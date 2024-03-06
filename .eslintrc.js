89bfdaecb1d4478981b25cbdc7c945c35b6287f0
SashaKuh <sashakuharchuk04@gmail.com>
2024-03-06 20:05:32 +0200

    last

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
